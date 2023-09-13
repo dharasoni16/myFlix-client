3bfe85ac71969081eb26667a0e12e8070ccc4dd3
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-09-13 16:05:42 +0200

    Add:UseEffect hook is added to fetch data from movie-api

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -21,11 +23,21 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>Director:</span>
         <span>{movie.director}</span>
       </div>
-      <div>
-        <span>Featured:</span>
-        <span>{movie.featured}</span>
-      </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie:PropTypes.shape({
+    title:PropTypes.string.isRequired,
+    description:PropTypes.string.isRequired,
+    genre:PropTypes.shape({
+      Name:PropTypes.string.isRequired
+    }),
+    director:PropTypes.shape({
+      Name:PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onBackClick:PropTypes.func.isRequired
 };
