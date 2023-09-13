903a6c60502f7a9a3c9d4353f855d9a2a1edd176
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-09-13 11:27:06 +0200

    Add:parcel-bundler as dev dependency for easy setup and run

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import  React, { useContext, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
